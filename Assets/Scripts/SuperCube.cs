a17c827862505f7bd71073b66c4175c4936f1f4d
TronSkiviRu <skutaligor@gmail.com>
2025-09-24 12:47:16 +0700

    что-другое

--- Assets/Scripts/SuperCube.cs
+++ Assets/Scripts/SuperCube.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class SuperCube : MonoBehaviour
 {
 
+    // Android development. LOL
+    //1
+    /// 2
     void FixedUpdate()
     {
         gameObject.transform.Rotate(new(0.1f, 0.2f, 0.3f));
     }
-    // Android development. LOL
 }
